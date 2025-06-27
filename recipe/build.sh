89b2dcc31099f9b297002ded042e5c47f3557cf6
H. Joe Lee <hyoklee@hdfgroup.org>
2025-06-27 14:15:32 -0500

    build: install cte

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,9 @@
+export CMAKE_PREFIX_PATH=$PREFIX
+git clone --single-branch https://github.com/hyoklee/vcpkg
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg install content-transfer-engine
+cd ..
 sh env.sh
 cmake -B build .
 make -C build -j
