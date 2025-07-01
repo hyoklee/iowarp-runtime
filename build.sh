a83b0621580c6a43988c59b757fe425a45cf6311
H. Joe Lee <hyoklee@hdfgroup.org>
2025-07-01 11:59:42 -0500

    build(conda): move meta up

---
+++ build.sh
@@ -0,0 +1,13 @@
+export CMAKE_PREFIX_PATH=$PREFIX
+export CMAKE_INCLUDE_PATH=$PREFIX/include
+export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+# git clone --single-branch https://github.com/hyoklee/vcpkg
+# cd vcpkg
+# ./bootstrap-vcpkg.sh
+# ./vcpkg install margo
+# cd ..
+sh env.sh
+cmake -B build .
+make -C build -j
+
+
